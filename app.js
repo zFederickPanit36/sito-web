c495eb4b0f49b5c88f9e645b16ba852699276a29
zFederickPanit36 <86781813+zFederickPanit36@users.noreply.github.com>
2022-04-14 21:20:22 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+const discordTag = document.getElementById("discord");
+
+discordTag.addEventListener("click", () => {
+    console.log("copied");
+    navigator.clipboard.writeText("zFederickPanit36#8160");
+});
